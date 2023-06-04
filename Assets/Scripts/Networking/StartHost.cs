fa6cf0ac23357073d88b62d4fd7978fd92a57ab7
Eleocraft <eliseo.cailloux@gmx.de>
2023-06-04 20:49:42 +0200

    Packages

--- Assets/Scripts/Networking/StartHost.cs
+++ Assets/Scripts/Networking/StartHost.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 
 public class StartHost : MonoBehaviour
 {
+    [SerializeField] private bool SimulatedDelay;
     void Start()
     {
         if (NetworkManager.Singleton.SceneManager == null)
+        {
             NetworkManager.Singleton.StartHost();
+            if (SimulatedDelay)
+                NetworkManager.Singleton.GetComponent<UnityTransport>().SetDebugSimulatorParameters(120, 5, 3);
+        }
         else
             Destroy(this.gameObject);
     }
