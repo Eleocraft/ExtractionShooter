c943ff2add3f87aab2384441eab70da76f81865b
Eleocraft <eliseo.cailloux@gmx.de>
2023-06-04 19:27:39 +0200

    supports DNS adresses

--- Assets/Scripts/Menus and UI/MainMenuController.cs
+++ Assets/Scripts/Menus and UI/MainMenuController.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine.SceneManagement;
+using System.Net;
 using TMPro;
+using System.Linq;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -15,9 +17,30 @@ public class MainMenuController : MonoBehaviour
     }
     public void StartClient()
     {
-        if (string.IsNullOrEmpty(IpInputField.text)) return;
+        if (string.IsNullOrEmpty(IpInputField.text))
+            IpInputField.text = "localhost";
 
-        NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = IpInputField.text;
+        NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = GetIPfromURL(IpInputField.text);
         NetworkManager.Singleton.StartClient();
+    }
+    private string GetIPfromURL(string URL)
+    {
+        if (!URL.Any(x => !char.IsLetter(x)))
+            return URL;
+        if (URL == "localhost")
+            return "127.0.0.1";
+        try 
+        {
+            IPHostEntry Hosts = Dns.GetHostEntry(URL);
+            return Hosts.AddressList[0].ToString();
+        }
+        catch
+        {
+            throw new WrongAdressException();
+        }
+    }
+    public class WrongAdressException : System.Exception
+    {
+        public WrongAdressException() {}
     }
 }
